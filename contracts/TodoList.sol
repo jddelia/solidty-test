5e6f16c0274894d9b8b5ed38075ba5ce1e30b628
jddelia <jddelia123@gmail.com>
2019-02-27 16:14:22 -0500

    Set up tests

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,21 @@ pragma solidity ^0.5.0;
 
 contract TodoLit {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Test dappuniversity");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
